e66b0bb99906d67e90b50eae4ae93a31a23c3ce8
PRACHI-RAY-5947 <149695789+PRACHI-RAY-5947@users.noreply.github.com>
2023-12-13 19:55:23 +0530

    Add files via upload

---
+++ 6-2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+main ()
+{
+	int a,b;
+	clrscr();
+
+	printf("frist number : ");
+	scanf("%d",&a);
+	printf("second number : ");
+	scanf("%d",&b);
+
+	if(a<b)
+	{
+		 printf("frist number is minimum number : ");
+
+	}
+	else
+	{
+		printf("second number is minimum number : ");
+	}
+
+	getch();
+}
